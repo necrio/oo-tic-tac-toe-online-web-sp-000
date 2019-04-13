9a28fbc2a6fe0f5038e18e72e55643bef8626cfc
Keith Isham <kisham8899@gmail.com>
2019-04-13 16:28:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,7 +91,7 @@ end
           WIN_COMBINATIONS.detect do |combo|
             board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(combo[0])
 
-end
+        end
 
 
 
