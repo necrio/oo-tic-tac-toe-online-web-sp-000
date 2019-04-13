3621503b5810176804fc20cf8eea481a21ff4aec
Keith Isham <kisham8899@gmail.com>
2019-04-13 15:51:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,6 +5,21 @@ class TicTacToe
 
   def initialize
     self.spaces = Array.new(9, " ")
+  end
+
+    def display
+   puts " #{self.spaces[0]} | #{self.spaces[1]} | #{self.spaces[2]} "
+   puts "-----------"
+   puts " #{self.spaces[3]} | #{self.spaces[4]} | #{self.spaces[5]} "
+   puts "-----------"
+   puts " #{self.spaces[6]} | #{self.spaces[7]} | #{self.spaces[8]} "
+ end
+
+
+
+
+
+
 
 
 end
