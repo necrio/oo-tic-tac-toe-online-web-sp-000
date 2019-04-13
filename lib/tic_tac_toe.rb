769870cecb55708b6961b026f1497b7282019ad7
Keith Isham <kisham8899@gmail.com>
2019-04-13 16:16:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,14 +64,14 @@ end
 
         def turn
         puts "Please enter 1-9:"
-      
-      
+
+
         user_input = gets.strip
-    
+
         index = input_to_index(user_input)
         token = current_player
 
-    
+
         if valid_move?(index)
         puts 'valid move'
         move(index, token)
