1383d279a120367c5471ef54bb5413cbe52a2653
Keith Isham <kisham8899@gmail.com>
2019-04-13 16:11:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,6 +43,11 @@ class TicTacToe
   !(@board[index].nil? || @board[index] == " ")
   end
 
+  def valid_move?(board, index)
+  index.between?(0, 8) && !(position_taken?(board, index))
+
+  end
+
 
 
 
