b75bd49a4e7ea9e7829c25c46cc51e8f3118689b
Keith Isham <kisham8899@gmail.com>
2019-04-13 16:35:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,7 +91,7 @@ end
           WIN_COMBINATIONS.detect do |combo|
             if (@board[combo[0]]) == "X" && (@board[combo[1]]) == "X" && (@board[combo[2]]) == "X"
               return combo
-            elsif 
+            elsif
               (@board[combo[0]]) == "O" && (@board[combo[1]]) == "O" && (@board[combo[2]]) == "O"
 
         end
